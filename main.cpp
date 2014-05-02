cfda8de8b9082397bab896c88899a85405fa14ca
Arjan <arjan@vidigo.tv>
2014-05-02 21:46:59 +0200

    now it compiles on vs11

--- main.cpp
+++ main.cpp
@@ -14,171 +14,205 @@ using namespace chrono;
 
 typedef function< void() > function_type;
 
+template < typename T >
+struct naive_queue
+{
+        naive_queue( size_t r ) :
+            lock_(),
+            data_(),
+            index_( 0 )
+        {
+            data_.reserve( r );
+        }
+
+        void push( const T &t )
+        {
+            lock_guard< mutex > guard( lock_ );
+            data_.push_back( t );
+        }
+
+        bool pop( T &t )
+        {
+            lock_guard< mutex > guard( lock_ );
+
+            if ( index_ == data_.size() ) return false;
+            t = data_[ index_++ ];
+            return true;
+        }
+
+        mutex lock_;
+        size_t index_;
+        vector< T > data_;
+};
+
 typedef lock_free::fifo< function_type > jobqueue;
+//typedef naive_queue< function_type > jobqueue;
 
 template < typename T >
 T to( const string &str )
 {
-	T result;
-	stringstream( str ) >> result;
-	return result;
+    T result;
+    stringstream( str ) >> result;
+    return result;
 }
 
 template < typename  T >
 function_type get_producer( T &&t )
 {
-	return get< 0 >( t );
+    return get< 0 >( t );
 }
 
 template < typename  T >
 function_type get_consumer( T &&t )
 {
-	return get< 1 >( t );
+    return get< 1 >( t );
 }
 
 template < typename  T >
 function_type get_result( T &&t )
 {
-	return get< 2 >( t );
+    return get< 2 >( t );
 }
 
 int main( int argc, char *argv[] )
 {
-	auto create_producer_consumer_result = []( const string &name )
-	{
-		struct data_type
-		{
-			data_type( size_t e ) :
-				expected( e ),
-				queue( 0 ),
-				producer_count( 0 ),
-				consumer_count( 0 ) { }
-			const size_t expected;
-			jobqueue queue;
-			atomic_size_t producer_count;
-			atomic_size_t consumer_count;
-		};
-		
-		high_resolution_clock::time_point t1 = high_resolution_clock::now();
-		
-		auto data = make_shared< data_type >( 1e6 );
-		
-		function_type producer = [data]()
-		{
-			while ( data->producer_count++ < data->expected )
-			{
-				data->queue.push(
-					[data]()
-					{
-						++data->consumer_count;
-					}
-				);
-			}
-			if ( data->producer_count >= data->expected )
-			{
-				--data->producer_count;
-			}
-		};
-		
-		function_type consumer = [data]()
-		{
-			while ( data->consumer_count < data->expected )
-			{
-				function_type func;
-				while ( data->queue.pop( func ) )
-				{
-					func();
-				}
-			}
-		};
-		
-		function_type result = [=]()
-		{
-			high_resolution_clock::time_point t2 = high_resolution_clock::now();
-			
-			duration< double > time_span = duration_cast< duration< double > >(t2 - t1);
-			
-			cout << "expected: " << data->expected << ", actual: " << data->consumer_count << endl;
-			cout << name << " took: " << time_span.count() << " seconds" << endl;
-		};
-		
-		return make_tuple( producer, consumer, result );
-	};
+    auto create_producer_consumer_result = []( const string &name )
+    {
+        struct data_type
+        {
+            data_type( size_t e ) :
+                expected( e ),
+                queue( e ),
+                producer_count( 0 ),
+                consumer_count( 0 ) { }
+            const size_t expected;
+            jobqueue queue;
+            atomic_size_t producer_count;
+            atomic_size_t consumer_count;
+        };
 
-	const auto thread_count = argc > 1 ? to< size_t >( argv[ 1 ] ) : 20;
+        high_resolution_clock::time_point t1 = high_resolution_clock::now();
 
-	// single producer, single consumer
-	{
-		auto pcr = create_producer_consumer_result( "single producer, single consumer" );
-		
-		get_producer( pcr )();
-		
-		get_consumer( pcr )();
-		
-		get_result( pcr )();
-	}
-	
-	// single producer, multi consumer
-	{
-		auto pcr = create_producer_consumer_result( "single producer, multi consumer" );
-		
-		get_producer( pcr )();
-		
-		vector< thread > threads;
-		size_t c = thread_count;
-		while ( c-- )
-		{
-			threads.push_back( thread( get_consumer( pcr ) ) );
-		}
-		
-		for ( auto &t : threads )
-		{
-			t.join();
-		}
-		
-		get_result( pcr )();
-	}
-	
-	// multi producer, single consumer
-	{
-		auto pcr = create_producer_consumer_result( "multi producer, single consumer" );
-		
-		vector< thread > threads;
-		size_t c = thread_count;
-		while ( c-- )
-		{
-			threads.push_back( thread( get_producer( pcr ) ) );
-		}
-		
-		for ( auto &t : threads )
-		{
-			t.join();
-		}
-		
-		get_consumer( pcr )();
-		
-		get_result( pcr )();
-	}
-	
-	// multi producer, multi consumer
-	{
-		auto pcr = create_producer_consumer_result( "multi producer, multi consumer" );
-		
-		vector< thread > threads;
-		size_t c = thread_count / 2;
-		while ( c-- )
-		{
-			threads.push_back( thread( get_producer( pcr ) ) );
-			threads.push_back( thread( get_consumer( pcr ) ) );
-		}
-		
-		for ( auto &t : threads )
-		{
-			t.join();
-		}
-		
-		get_result( pcr )();
-	}
+        auto data = make_shared< data_type >( 1e6 );
 
-	return 0;
+        function_type producer = [data]()
+        {
+            while ( data->producer_count++ < data->expected )
+            {
+                data->queue.push(
+                    [data]()
+                    {
+                        ++data->consumer_count;
+                    }
+                );
+            }
+            if ( data->producer_count >= data->expected )
+            {
+                --data->producer_count;
+            }
+        };
+
+        function_type consumer = [data]()
+        {
+            while ( data->consumer_count < data->expected )
+            {
+                function_type func;
+                while ( data->queue.pop( func ) )
+                {
+                    func();
+                }
+            }
+        };
+
+        function_type result = [=]()
+        {
+            high_resolution_clock::time_point t2 = high_resolution_clock::now();
+
+            duration< double > time_span = duration_cast< duration< double > >(t2 - t1);
+
+            cout << "expected: " << data->expected << ", actual: " << data->consumer_count << endl;
+            cout << name << " took: " << time_span.count() << " seconds" << endl;
+        };
+
+        return make_tuple( producer, consumer, result );
+    };
+
+    const auto thread_count = argc > 1 ? to< size_t >( argv[ 1 ] ) : 16;
+
+    // single producer, single consumer
+    {
+        auto pcr = create_producer_consumer_result( "single producer, single consumer" );
+
+        get_producer( pcr )();
+
+        get_consumer( pcr )();
+
+        get_result( pcr )();
+    }
+
+    // single producer, multi consumer
+    {
+        auto pcr = create_producer_consumer_result( "single producer, multi consumer" );
+
+        get_producer( pcr )();
+
+        vector< thread > threads;
+        size_t c = thread_count;
+        while ( c-- )
+        {
+            threads.push_back( thread( get_consumer( pcr ) ) );
+        }
+
+        for ( auto &t : threads )
+        {
+            t.join();
+        }
+
+        get_result( pcr )();
+    }
+
+    // multi producer, single consumer
+    {
+        auto pcr = create_producer_consumer_result( "multi producer, single consumer" );
+
+        vector< thread > threads;
+        size_t c = thread_count;
+        while ( c-- )
+        {
+            threads.push_back( thread( get_producer( pcr ) ) );
+        }
+
+        for ( auto &t : threads )
+        {
+            t.join();
+        }
+
+        get_consumer( pcr )();
+
+        get_result( pcr )();
+    }
+
+    // multi producer, multi consumer
+    {
+        auto pcr = create_producer_consumer_result( "multi producer, multi consumer" );
+
+        vector< thread > threads;
+        size_t c = thread_count / 2;
+        while ( c-- )
+        {
+            threads.push_back( thread( get_producer( pcr ) ) );
+            threads.push_back( thread( get_consumer( pcr ) ) );
+        }
+
+        for ( auto &t : threads )
+        {
+            t.join();
+        }
+
+        get_result( pcr )();
+    }
+
+//    getc(stdin);
+
+    return 0;
 }
